82cca461eab5252c16aff4750f90a615e5ca315e
Asi777 <asafmarom89@gmail.com>
2020-09-11 13:52:09 +0300

    send

--- dist/render.js
+++ dist/render.js
@@ -1,12 +1,22 @@
 class Renderer {
     
     renderData(allCityData) {
-        $('#list').empty()
+        $('#main').empty()
         const source = $(`#first-template`).html();
         const template = Handlebars.compile(source);
         let newHTML = template({allCityData});
-         $('#list').append(newHTML);
+         $('#main').append(newHTML);
 
     }
+
+    // renderData22(allCityData) {
+    //     $('#list').empty()
+    //     const source = $(`#second-template`).html();
+    //     const template = Handlebars.compile(source);
+    //     let newHTML = template({allCityData});
+    //      $('#list').append(newHTML);
+
+
+    // }
     
 }
